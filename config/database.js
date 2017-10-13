aced3f652499931177b5c0bff1fe99f19561b617
Davo <davo@MBP-Davo.home>
2017-10-13 00:55:26 -0300

    Layout update

--- config/database.js
+++ config/database.js
@@ -4,10 +4,6 @@ var envConfig = {
         url: 'mongodb://localhost:27017/sc-baseapp'
     },
 
-    heroku: {
-        url: ''
-    },
-
     production: {
         url: ''
     }
--- config/oauth.js
+++ config/oauth.js
@@ -2,28 +2,19 @@ var envConfig = {
 
     development: {
         site_url: 'http://localhost:8080',
-        hub_url: 'http://localhost:1337',
-        client_id: 'WUW7ZSW4IR',
-        client_secret: 'YX6sWKURZDX4lGpnEsKSelvw2H3UMR',
+        hub_url: 'http://lab.simcase.io/',
+        client_id: 'Z6JEQJ659M',
+        client_secret: 'NReaaG9B0iRT5uawid2LJLstj2WRbB',
         redirect_uri: 'http://localhost:8080/oauth_callback',
         scope: 'player'
     },
 
-    heroku: {
-        site_url: 'http://sc-optimization.herokuapp.com',
-        hub_url: 'http://simcase-hub.herokuapp.com',
-        client_id: '9NAY74T7FJ',
-        client_secret: 'FMEtNhQNZSYi0w8s9xlRTuj1CTRQ5f',
-        redirect_uri: 'http://sc-optimization.herokuapp.com/oauth_callback',
-        scope: 'player'
-    },
-
     production: {
-        site_url: 'http://lab.simcase.io/apps/optimization',
+        site_url: 'http://lab.simcase.io/apps/baseapp',
         hub_url: 'http://lab.simcase.io/',
-        client_id: '9NAY74T7FJ',
-        client_secret: 'FMEtNhQNZSYi0w8s9xlRTuj1CTRQ5f',
-        redirect_uri: 'http://lab.simcase.io/apps/optimization/oauth_callback',
+        client_id: '',
+        client_secret: '',
+        redirect_uri: 'http://lab.simcase.io/apps/baseapp/oauth_callback',
         scope: 'player'
     }
 
